bc35f165743f3ce3db29d12a4b85fb369a487674
vaibhav pal <124390229+Vaibha3246@users.noreply.github.com>
2026-04-20 23:17:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to my world /n consisitency is the key of succes🚀"
+    return "Welcome to my world /n consisitency is the key of succes  and i want become a devops mlops enginer🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
