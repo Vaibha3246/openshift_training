cbe966636dad28c81972cc5e658a89ad4d13aa0e
vaibhav pal <124390229+Vaibha3246@users.noreply.github.com>
2026-04-20 22:52:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to my world 🚀"
+    return "Welcome to my world /n consisitency is the key of succes🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
